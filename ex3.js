ac0d66d4fc6f4de1ec09c3a33161c61615e12ded
Jordforb@gmail.com <Jordanforbes1@Jordans-MacBook-Pro-2.local>
2017-01-10 14:34:59 -0500

    name change, added ex3

---
+++ ex3.js
@@ -0,0 +1,1 @@
+var my_arr = ["giraffe","zebra","antelope"];
--- guitar.js
+++ guitar.js
@@ -1,3 +1,5 @@
-var notes= [c, cs, d, ds, e, f, fs, g, gs, a, as, b]
-var majorScale[notes[i]]= notes[0,2,4,5,7,8,9]
-var triChord[majorScale]= majorScale[0,2,4]
+var notes= ["c", "cs", "d", "ds", "e", "f", "fs", "g", "gs", "a", "as", "b"]
+var scale= notes[[0,2,4,5,7,8,9,11],[0,2,3,5,7,9,10],[0,1,3,5,7,8,10],
+                  [0,2,4,5,7,8,10,11],[0,2,4,5,7,9,10],[0,2,3,5,7,9,10],[0,1,3,5,6,8,10]]
+var majorTri= [scale([0][0]), scale([0][2]), scale([0][4]),]
+var majorSev= majorTri + scale([0][6])
