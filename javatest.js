7846b33b31682fa1b366179fb2d428d8fea8d11d
Jordforb@gmail.com <Jordanforbes1@Jordans-MacBook-Pro-2.local>
2017-01-10 11:51:54 -0500

    javascript change

--- javatest.js
+++ javatest.js
@@ -1,6 +1,14 @@
 var z = 3;
+var c = 10;
 
-function some_f(z){
-  console.log(z);
+
+
+function hallo(){
+  if(true){
+    var c = 2;
+  }
+  console.log(c);
+  // returns 2, not the global value 10
+  // implication: variables don't have a
+  // separate scope within a block
 }
-some_f(10)
