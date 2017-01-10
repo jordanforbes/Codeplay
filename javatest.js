d60c99cf2ccd6ad19515576f8899f0fb11500ede
Jordforb@gmail.com <Jordanforbes1@Jordans-MacBook-Pro-2.local>
2017-01-10 12:01:56 -0500

    new javascript

--- javatest.js
+++ javatest.js
@@ -1,14 +1,5 @@
-var z = 3;
-var c = 10;
-
-
-
-function hallo(){
-  if(true){
-    var c = 2;
-  }
-  console.log(c);
-  // returns 2, not the global value 10
-  // implication: variables don't have a
-  // separate scope within a block
+function addNums(){
+  num1 = document.getElementById('num1').value;
+  num2 = document.getElementById('num2').value;
+  document.getElementById('result').innerHTML=(parseInt(num1) + parseInt(num2));
 }
