ee6b1c0d072403cc5d30e1217e3f05b504e739c7
Jordforb@gmail.com <Jordanforbes1@Jordans-MacBook-Pro-2.local>
2017-01-11 10:47:33 -0500

    door game

--- minichallenges.js
+++ minichallenges.js
@@ -122,3 +122,51 @@ pickDoor()
 //(e.g. problem 8 requires you to alert
 //the user to what door they selected and what
 //the prize is.)
+
+/*
+function pickDoor(door){
+  if (door == 1){
+    return alert('You selected door number 1! Congratulations you won a jetski!')
+  } else if ( door == 2){
+    return alert('You selected door number 2! congratulations you won a bottle of water!')
+  } else if ( door == 3){
+    return alert('You selected door number 3! congratulations you won nothing!')
+  } else if ( door > 3 && door <= 20){
+    return alert('nope theres only three doors friendo')
+  } else if ( door > 20){
+    return alert ('what the hell are you doing?')
+  } else if ( door == 0){
+    return alert ('Oh come on dont be like that')
+  }
+}
+
+pickDoor()
+*/
+
+alert("You have three doors numbered 1 through 3, pick a door and win a prize!");
+var doors = prompt("pick a door!");
+switch(doors){
+  case '1':
+  ('You\'ve selected door number 1! Congratulations you won a jetski!');
+  break;
+  case '2':
+  alert('You\'ve selected door number 2! congratulations you won a bottle of water!')
+  break;
+  case '3':
+  alert('You\'ve selected door number 3! congratulations you won nothing!')
+  break;
+  case '69':
+  alert("you've won the true prize")
+  break;
+  case '420':
+  alert('shit my dude take a hit of this danky dank')
+  break;
+  case'no':
+  alert('fuck you')
+  break;
+  default:
+    alert('Oh come on dont be like that')
+}
+
+//how can I make this into a text input on the page?
+//how can I make this loop until you select a correct door number?
